8b019e8ba52cc17bc411970ec4c6646bb995bbde
antares-va-tech <antares.va.tech@gmail.com>
2021-01-18 12:11:07 -0500

    enabled redux + commented code causing compile err

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,39 +2,39 @@ import { combineReducers, createStore } from "redux"
 import { combineEpics } from "redux-observable"
 import { composeWithDevTools } from "redux-devtools-extension"
 
-import SnLoaderReducer from "./sn.loader.reducer"
-import SnPerson from "./sn.person.reducer"
-import SnUserProfile from "./sn.userprofile.reducer"
-import { snUserProfileEpic } from "./sn.userprofile.epic"
-import SnUserMasterProfile from "./sn.usermasterprofile.reducer"
-import { snUserMasterProfileEpic } from "./sn.usermasterprofile.epic"
-import SnMyFollowers from "./sn.myFollowers.reducer"
-import { snMyFollowersEpic } from "./sn.myFollowers.epic"
-import SnMyFollowings from "./sn.myFollowings.reducer"
-import { snMyFollowingsEpic } from "./sn.myFollowings.epic"
-import SnUploadListReducer from "./SnUploadList.reducer";
+// import SnLoaderReducer from "./sn.loader.reducer"
+// import SnPerson from "./sn.person.reducer"
+// import SnUserProfile from "./sn.userprofile.reducer"
+// import { snUserProfileEpic } from "./sn.userprofile.epic"
+// import SnUserMasterProfile from "./sn.usermasterprofile.reducer"
+// import { snUserMasterProfileEpic } from "./sn.usermasterprofile.epic"
+// import SnMyFollowers from "./sn.myFollowers.reducer"
+// import { snMyFollowersEpic } from "./sn.myFollowers.epic"
+// import SnMyFollowings from "./sn.myFollowings.reducer"
+// import { snMyFollowingsEpic } from "./sn.myFollowings.epic"
+import SnUploadListReducer from "./action-reducers-epic/SnUploadListReducer";
 
 const redux = require("redux")
 const { createEpicMiddleware } = require("redux-observable")
 
 const rootReducer = combineReducers({
-  snLoader: SnLoaderReducer,
+  // snLoader: SnLoaderReducer,
   // userSession: SnUserSessionReducer,
-  person: SnPerson,
-  snUserProfile: SnUserProfile,
-  snUserMasterProfile: SnUserMasterProfile,
-  snMyFollowers: SnMyFollowers,
-  snMyFollowings: SnMyFollowings,
+  // person: SnPerson,
+  // snUserProfile: SnUserProfile,
+  // snUserMasterProfile: SnUserMasterProfile,
+  // snMyFollowers: SnMyFollowers,
+  // snMyFollowings: SnMyFollowings,
   snUploadListStore: SnUploadListReducer
 });
 
 const rootEpic = combineEpics(
   // snPersonEpic,
   // logoutPersonEpic,
-  snUserProfileEpic,
-  snUserMasterProfileEpic,
-  snMyFollowersEpic,
-  snMyFollowingsEpic
+  // snUserProfileEpic,
+  // snUserMasterProfileEpic,
+  // snMyFollowersEpic,
+  // snMyFollowingsEpic
 )
 
 const observableMiddleware = createEpicMiddleware()
