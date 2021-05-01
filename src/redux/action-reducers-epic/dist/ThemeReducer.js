636f2951a511f4d16bde571998c6e5bbe5076560
Usama Sadiq <quban54@gmail.com>
2021-05-02 03:26:55 +0500

    some changes in app

--- src/redux/action-reducers-epic/dist/ThemeReducer.js
+++
@@ -1,9 +0,0 @@
-const Darkmode;
-export default (state = null, action) => {
-    switch (action.type) {
-      case Darkmode:
-        return action.payload
-      default:
-        return state
-    }
-  }
