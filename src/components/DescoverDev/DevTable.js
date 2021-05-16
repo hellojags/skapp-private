a344b1d8e1299d4e59d09650ee533d8ef76af859
Md Shah Ali <shahalihridoy@gmail.com>
2021-05-16 22:14:29 +0600

    multipel user with same id problem fixed

--- src/components/DescoverDev/DevTable.js
+++ src/components/DescoverDev/DevTable.js
@@ -322,25 +322,26 @@ const DevTable = ({
       (pageNumber + 1) * userPerPage
     );
 
-    const promiseList = idList.map(async (id) => {
-      let [profile = {}, following, appCount] = await Promise.all([
-        getProfile(id),
-        getFollowingCountForUser(id),
-        getUsersPublishedAppsCount(id),
-      ]);
+    const promiseList = idList.map(
+      async (id) =>
+        await Promise.all([
+          getProfile(id),
+          getFollowingCountForUser(id),
+          getUsersPublishedAppsCount(id),
+          Promise.resolve(id),
+        ])
+    );
 
-      if (!profile) {
-        profile = {};
-      }
+    const results = await Promise.all(promiseList);
 
-      profile.following = following || 0;
-      profile.appCount = appCount || 0;
-      profile.uid = id;
+    const list = results.map((item) => {
+      const profile = item[0] || {};
+      profile.following = item[1] || 0;
+      profile.appCount = item[2] || 0;
+      profile.uid = item[3];
 
-      return profile;
+      return { ...profile };
     });
-
-    const list = await Promise.all(promiseList);
 
     pageNumberRef.current = pageNumber + 1;
     prevPageNumberRef.current = pageNumber;
@@ -433,7 +434,7 @@ const DevTable = ({
                     />
                   </TableCell>
                   <TableCell>
-                    {item.avatar && item.avatar[0] ? (
+                    {item.avatar && item.avatar[0] && item.avatar[0].url ? (
                       <img
                         className={classes.devAvtar}
                         src={transformImageUrl(item.avatar[0].url)}
@@ -506,27 +507,3 @@ const DevTable = ({
 };
 
 export default DevTable;
-
-const dummyList = [
-  "00ccb974591621494f2c4302261c13dfac017f3ff9206c657713ec5f3ecb52ff",
-  "d88144039b7e605de05836febb0d184899b7556133e0bd568b56d54f1545fe69",
-  "6873b70f00d1234124dc55cc1e1642e0c43804445a113ae9c76c15e38a5e0571",
-  "c39da7010692d55a6550aebd6f5f2b2a23e7d26280621367a447628c544053e3",
-  "5a27a1c4eec1cb0a55d910ce0eff4073f7ddd58d37de0a64f92c2356d61a8a94",
-  "4bd7c9b9d3fc03dfcdbd14590fac9e6f97801c87a38bfcc262d86fba5b9439a6",
-  "570980a7f24391a9ced450cd8f22a9d78229c650ad24b7c2686b5bb86915418e",
-  "3d4e50cfe857d94403c21f38be21073ecc42c7c828101e26c7628fd0b6fad67f",
-  "8b8544d54ecf56da6be887232361eec9f524429c1bd523f4778b20fb9945d15c",
-  "d21eb9d8d38e7b495cc47b94a046eab710edf7f1b19d42d5f1b201feb3406a2a",
-  "22f91386b2e341edb046ff880a2e817b3b70fdd958113dc93b9b1375880dd5d2",
-  "c25858373033e730a5e592cb5fd5b5fa90657da06210886c1f30552796973cb9",
-  "73a83de68f07d77a75f3e8d7534f58c2d0a613aeffa2ec4f53238ee5af5a3379",
-  "4294b7224a3d19a75abf7970f1bf3213c0370ea36d36a689cbd39e53333ec7f4",
-  "5dc982eed6290fbe02f7781ec92051ef12e835a0565885eacfc94a9ee07686f0",
-  "b85e1cd34633297d6004446f935d220918a8e2c5b98a5f5cc32c3c6c93f72d6b",
-  "2b02efca9ed51cfed5c645eb3c1513d9343207a9e843454de72771e57c805d48",
-  "403a35ed6b473518a213d514c3d105471d4bb454b67e4c4db106f061c13cb9a3",
-  "dfa6e4e25be41cfe27a4457fab9a162db425cc7d230ff14370f9ae2a86f3a0ec",
-  "c4b99808f188174c54edcc3cb1f2b864966911f15682d6fcdf728657c7813a30",
-  "ce2df8006eb4a0179a5b1f85a59688b3749bffca91984614b40454dfa7ce3d3c",
-];
